a3d908cd26345331d9ba6d674279216ddd328cd3
Aze-M <archmageazekil@gmail.com>
2022-11-22 14:32:49 +0100

    Blog grab added, readability ensured with regex. Refresh button works correctly.

--- App.js
+++ App.js
@@ -1,37 +1,123 @@
 import { StatusBar } from 'expo-status-bar';
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import { ScrollView, StyleSheet, Text, View, ViewBase, Button, Pressable } from 'react-native';
 
-const blogPost = {
-  title: 'Wow, I am a Test! WowWowWowWowWowWowWowWowWowWowWowWowWowWowWowWowWowWowWowWowWowWowWowWowWowWowWowWow',
-  content: 'This is so cool wow, all the cool stuff like this can be read.',
-  date: '2022-11-22'
-}
+const mainCLR = '#5C1868';
+const mainContentCLR = '#1A1A1A';
+const neutralBgCLR = '#fff';
+const neutralTextBgCLR = '#404040';
+const mainTextCLR = 'black';
+const postTextCLR = 'white';
+let buttonBgCLR = '#5C1868';
+let buttonBgCLRPressed = '#5D1969';
 
 const postStyle = StyleSheet.create({
   container: {
-    color: 'white',
+    color: postTextCLR,
+    marginVertical: 16,
   },
   title: {
+    paddingLeft: 16,
     height: 48,
-    fontSize: 42,
-    color: 'white',
-    backgroundColor: '#5C1868',
-    overflow: 'hidden'
+    fontSize: 36,
+    color: postTextCLR,
+    backgroundColor: mainCLR,
+    overflow: 'hidden',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
   },
   content: {
+    paddingLeft: 16,
     fontSize: 30,
-    color: 'white',
-    backgroundColor: '#1A1A1A'
+    color: postTextCLR,
+    backgroundColor: mainContentCLR
   },
   date: {
+    paddingRight: 16,
     textAlign: 'right',
     fontSize: 24,
     height: 48,
-    color: 'white',
-    backgroundColor: '#404040'
+    color: postTextCLR,
+    backgroundColor: neutralTextBgCLR,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
   }
 })
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    height: 100,
+    marginBottom: 5,
+    backgroundColor: neutralBgCLR,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  body: {
+    flex: 1,
+    backgroundColor: neutralBgCLR,
+  },
+  footer: {
+    height: 80,
+    backgroundColor: neutralBgCLR,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  footerButton: {
+    height: 60,
+    paddingHorizontal: 20,
+    backgroundColor: buttonBgCLR,
+    borderRadius: 10,
+    width: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  baseText: {
+    fontSize: 32,
+    textAlign: 'center',
+    color: mainTextCLR,
+    white: {
+      fontSize: 32,
+      textAlign: 'center',
+      color: postTextCLR,
+    }
+  },
+  headText: {
+    fontSize: 48,
+    color: mainTextCLR,
+  }
+});
+
+function parseHTMLtoEXPO(html) {
+  const RGXbrp = /(<br|<p)[^>]*>./g
+  const RGXAny = /<[^>]*>./g
+
+  let step_1 = html.replace(RGXbrp, "\n")
+
+  let step_2 = step_1.replace(RGXAny, "")
+
+
+  return step_2
+}
+
+class PostContainer extends Component {
+
+  componentDidMount() {
+    this.props.gen()
+  }
+
+  render() {
+    return (
+      <ScrollView style={styles.body}>
+        {this.props.posts}
+      </ScrollView>
+    )
+  }
+}
+
 
 class Post extends Component {
 
@@ -43,7 +129,7 @@ class Post extends Component {
         </Text>
         <View>
           <Text style={postStyle.content}>
-            {this.props.content}
+            {parseHTMLtoEXPO(this.props.content)}
           </Text>
         </View>
         <Text style={postStyle.date}>
@@ -55,53 +141,34 @@ class Post extends Component {
 }
 
 export default function App() {
+  const [posts, setPosts] = useState([])
+
+  function genNewPosts() {
+    setPosts([])
+    fetch('https://blg.aze-m.com/posts')
+      .then((res) => {
+        console.log(res)
+        return res.json()
+      }, () => { return new Error("Could not get Posts.") })
+      .then((json) => {
+        console.log(json)
+        tmp = json.map((elem, idx) => {
+          return <Post title={elem.title} content={elem.content} date={elem.date} key={idx} />
+        })
+        setPosts(tmp)
+        console.log(posts);
+      }, () => { return new Error("Could not generate Posts.") })
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headText}>Aze-M Blog Reader</Text>
       </View>
-      <ScrollView style={styles.body}>
-        <Post title={blogPost.title} content={blogPost.content} date={blogPost.date}></Post>
-      </ScrollView>
+      <PostContainer gen={genNewPosts} posts={posts} />
       <View style={styles.footer}>
-        <Pressable style={styles.footerButton}><Text style={styles.baseText}>Refresh</Text></Pressable>
+        <Pressable style={({pressed}) => [{backgroundColor: pressed ? buttonBgCLRPressed : buttonBgCLR},styles.footerButton]} onPress={() => genNewPosts()}><Text style={styles.baseText.white} color={postTextCLR}>Refresh</Text></Pressable>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  header: {
-    height: 100,
-    marginBottom: 5,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-  },
-  body: {
-    flex: 1,
-    backgroundColor: '#4a4a4a',
-  },
-  footer: {
-    height: 80,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  footerButton: {
-    height: 60,
-    paddingHorizontal: 'auto',
-    backgroundColor: '#5C1868',
-  },
-  baseText: {
-    fontSize: 32,
-    marginVertical: 16,
-    textAlign: 'center',
-  },
-  headText: {
-    fontSize: 48,
-  }
-});
